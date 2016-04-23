2f99d2629dc95c3ca6f3225b1f7ca080fc99d966
KaotikS <satasuarez@gmail.com>
2016-04-22 21:22:49 -0300

    Deleted setupAirport() method from Airport.java

--- TestApp/app/src/main/java/com/sata/testapp/classes/Airport.java
+++ TestApp/app/src/main/java/com/sata/testapp/classes/Airport.java
@@ -83,47 +83,4 @@ public class Airport implements Serializable {
     public void setCountry(String country) {
         this.country = country;
     }
-
-    public void setupAirport(String iata){
-        if(iata.equals("eze")){
-            this.latitude = -34.820898;
-            this.longitude = -58.534321;
-            this.formalName = "Aeropuerto Internacional Ministro Pistarini (Ezeiza)";
-            this.city = "CABA";
-            this.state = "Buenos Aires";
-            this.country = "Argentina";
-        }
-        if(iata.equals("mdq")){
-            this.latitude = -37.933844;
-            this.longitude = -57.583245;
-            this.formalName = "Aeropuerto Internacional Ástor Piazzolla";
-            this.city = "Mar del Plata";
-            this.state = "Buenos Aires";
-            this.country = "Argentina";
-        }
-        if(iata.equals("cor")){
-            this.latitude = -31.314184;
-            this.longitude = -64.214145;
-            this.formalName = "Aeropuerto Internacional Ingeniero Ambrosio Taravella";
-            this.city = "Córdoba";
-            this.state = "Córdoba";
-            this.country = "Argentina";
-        }
-        if(iata.equals("ush")){
-            this.latitude = -54.841910;
-            this.longitude = -68.308319;
-            this.formalName = "Aeropuerto Internacional Malvinas Argentinas";
-            this.city = "Ushuaia";
-            this.state = "Tierra del fuego";
-            this.country = "Argentina";
-        }
-        if(iata.equals("mns")){
-            this.latitude = -25.737254;
-            this.longitude = -54.473299;
-            this.formalName = "Aeropuerto Internacional Cataratas del Iguazú";
-            this.city = "Puerto Iguazú";
-            this.state = "Misiones";
-            this.country = "Argentina";
-        }
-    }
 }
